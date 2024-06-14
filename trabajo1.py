fd0e8b5977031ef7689a8c74169741c06310f47a
javier rojas <javie.rojasc@duocuc.cl>
2024-06-14 11:05:24 -0400

    ejercicio solo con menu

---
+++ trabajo1.py
@@ -0,0 +1,22 @@
+import os,time
+
+
+while True:
+    os.system('cls')
+    print("menu de trabajadores")
+    print("""1.registrar trabajador
+             2.lista de todos los trabajadores
+             3.imprimir sueldos en la pantalla
+             4.salir de programa""")
+    opc = int(input("ingrese la opcion:"))
+    os.system('cls')
+    if opc == 1:
+        pass
+    elif opc == 2:
+        pass
+    elif opc ==3:
+        pass
+    else:
+        print("adios")
+        break
+    time.sleep(3)
